361b38016f3d5a3ccb6fd922941fadf1110393d7
HappyBird6 <wsbg@naver.com>
2024-07-21 06:28:16 +0900

    구글로그인 수정

--- src/main/java/play/dpl/playlist/Service/LoginService.java
+++ src/main/java/play/dpl/playlist/Service/LoginService.java
@@ -10,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import play.dpl.playlist.Entity.Member;
+
 @Service
 public class LoginService {
     private final WebClient webClient;
@@ -22,8 +23,6 @@ public class LoginService {
     String redirectUri;
     @Value("${oauth2.google.token-uri}")
     String tokenUri;
-    @Value("${oauth2.google.resource-uri}")
-    String resourceUri;
 
     public LoginService(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.build();
@@ -34,22 +33,21 @@ public class LoginService {
 
         JsonNode userInfo = getUserInfo(accessToken);
         String email = userInfo.get("email").asText();
-        
+
         return Member.builder()
-                    .email(email)
-                    .accessCode(accessToken)
-                    .build();
+                .email(email)
+                .accessCode(accessToken)
+                .build();
     }
 
     public String getAccessToken(String authorizationCode) {
-        String tokenUrl = "https://oauth2.googleapis.com/token";
-
-        return webClient.post()
-                .uri(tokenUrl)
+        System.out.println("getAccessToken 시작");
+        String accessToken = webClient.post()
+                .uri(tokenUri)
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters.fromFormData("code", authorizationCode)
-                        .with("access_type","offline")
-                        .with("approval_prompt","force")
+                        .with("access_type", "offline")
+                        .with("approval_prompt", "force")
                         .with("client_id", clientId)
                         .with("client_secret", clientSecret)
                         .with("redirect_uri", redirectUri)
@@ -57,18 +55,23 @@ public class LoginService {
                 .retrieve()
                 .bodyToMono(JsonNode.class)
                 // 일단 리프레쉬 토큰 제외
-                // .map(jsonNode -> new String[]{jsonNode.get("access_token").asText(),jsonNode.get("refresh_token").asText()});
+                // .map(jsonNode -> new
+                // String[]{jsonNode.get("access_token").asText(),jsonNode.get("refresh_token").asText()});
                 .map(jsonNode -> jsonNode.get("access_token").asText())
                 .block();
+        System.out.println("getAcessToken : " + accessToken);
+        return accessToken;
     }
+
     public JsonNode getUserInfo(String accessToken) {
         String userInfoUrl = "https://www.googleapis.com/oauth2/v1/userinfo";
-        
-        return webClient.get()
+        JsonNode temp = webClient.get()
                 .uri(userInfoUrl)
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
                 .retrieve()
                 .bodyToMono(JsonNode.class)
                 .block();
+                
+        return temp;
     }
 }
